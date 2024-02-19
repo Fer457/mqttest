47eba3502426e61ae8555a228a653d24b448169d
SlevinKlvra <caragones.cas@gmail.com>
2024-02-19 17:06:59 +0100

    Forward Successfull

--- backend/io.js
+++ backend/io.js
@@ -1,12 +1,35 @@
+const axios = require('axios');
+
+// Reemplaza 'usuario' y 'contraseña' con tus credenciales reales
+const username = 'intecfull';
+const password = 'intecfullpassword';
+
+// Codifica las credenciales en base64
+const base64Credentials = Buffer.from(`${username}:${password}`).toString('base64');
+
 const socketConnection = (io) => {
   io.on("connection", (socket) => {
     console.log("User connected", socket.id);
 
-    socket.on("send_message", async (data) => {
-      socket.to(data.room).emit("receive_message", data);
-    });
     socket.on("join_room", (data) => {
       console.log(`User with id ${socket.id} is sending requests`);
+
+      const postData = {
+        action: "forward",
+      };
+
+      // Añade el encabezado de autorización a tu petición
+      axios.post('http://10.14.0.165:1880/endpoint/api/robot/control', postData, {
+        headers: {
+          'Authorization': `Basic ${base64Credentials}`
+        }
+      })
+      .then(response => {
+        console.log('Petición POST exitosa', response.data);
+      })
+      .catch(error => {
+        console.log('Error al realizar la petición POST', error.message);
+      });
     });
 
     socket.on("disconnect", () => {
