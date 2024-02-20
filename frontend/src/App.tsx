fcd41666847cc5fb13aeb22c5958e0a45e8105d4
Fer457 <fernandonevothernandez@gmail.com>
2024-02-20 01:23:28 +0100

    added tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import io, { Socket } from "socket.io-client";
+
 const socket: Socket = io("http://localhost:3001");
 
 const ChatPage = () => {
@@ -18,14 +19,24 @@ const ChatPage = () => {
   };
 
   return (
-    <div>
-      <button
-        onMouseDown={handleMouseDown}
-        onMouseUp={handleMouseUp}
-        onMouseLeave={handleMouseUp}
-      >
-        SALA
-      </button>
+    <div className="flex justify-center items-center h-screen bg-slate-300">
+      <div className="flex mt-4 bg-[#00567d] relative w-40 h-36 p-4">
+        <button
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+          onMouseLeave={handleMouseUp}
+          type="button"
+          className="w-0 h-0 border-l-[25px] border-r-[25px] border-b-[35px] border-solid border-l-transparent border-r-transparent border-b-white  bg-no-repeat bg-contain absolute top-2 left-1/2 -translate-x-1/2"
+        ></button>
+        <button
+          type="button"
+          className="w-0 h-0 border-t-[25px] border-b-[25px] border-r-[35px] border-solid border-t-transparent border-b-transparent border-r-white bg-no-repeat bg-contain absolute left-2 top-1/2 -translate-y-1/2"
+        ></button>
+        <button
+          type="button"
+          className="w-0 h-0 border-t-[25px] border-b-[25px] border-l-[35px] border-solid border-t-transparent border-b-transparent border-l-white bg-no-repeat bg-contain absolute right-2 top-1/2 -translate-y-1/2"
+        ></button>
+      </div>
     </div>
   );
 };
